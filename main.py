e444a338fcaade7b0f01c49e8e13ca0e497beea7
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-02-10 23:48:14 +0600

    Test 9

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-"""import pyshorteners
+import pyshorteners
 
 link=input('Enter The Big Ling For Short : ')
 shortener=pyshorteners.Shortener()
 x=shortener.tinyurl.short(link)
 
-print('\n\n\nThe Short Link Is : ',x)"""
+print('\n\n\nThe Short Link Is : ',x)
 
-a = 'Shantonu'
